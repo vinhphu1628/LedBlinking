a005c3ee3fcf0e49a4c9ad5f4582c78dd81b8d34
vinhphu1628 <meocondeptrai9999@yahoo.com.vn>
2018-10-17 09:44:26 +0700

    reupdate

--- app/src/main/java/com/example/xfoodz/ledblinking/MainActivity.java
+++ app/src/main/java/com/example/xfoodz/ledblinking/MainActivity.java
@@ -7,8 +7,10 @@ import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManager;
+import com.google.android.things.pio.Pwm;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -16,14 +18,40 @@ public class MainActivity extends Activity {
 
     private Handler mHandler = new Handler();
     private Gpio mLedGpio;
+    private Gpio mLedGpio1;
+    private Gpio mLedGpio2;
     private boolean mLedState = false;
+
+    private static final String PWM_NAME = "PWM1";
+    private Pwm mPwm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PeripheralManager manager = PeripheralManager.getInstance();
+        List<String> portList = manager.getPwmList();
+        if (portList.isEmpty()) {
+            Log.i(TAG, "No PWM port available on this device.");
+        } else {
+            Log.i(TAG, "List of available ports: " + portList);
+        }
+
+        try {
+            mPwm = manager.openPwm(PWM_NAME);
+            initializePwm(mPwm);
+        } catch (IOException e) {
+            Log.w(TAG, "Unable to access PWM", e);
+        }
+
         try{
-            String pinName = "BCM6";
+            String pinName = "BCM26";
             mLedGpio = PeripheralManager.getInstance().openGpio(pinName);
             mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            mLedGpio1 = PeripheralManager.getInstance().openGpio("BCM12");
+            mLedGpio1.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            mLedGpio2 = PeripheralManager.getInstance().openGpio("BCM16");
+            mLedGpio2.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
             mHandler.post(mBlinkRunnable);
         }
         catch(IOException e){
@@ -42,6 +70,15 @@ public class MainActivity extends Activity {
         } finally {
             mLedGpio = null;
         }
+
+        if (mPwm != null) {
+            try {
+                mPwm.close();
+                mPwm = null;
+            } catch (IOException e) {
+                Log.w(TAG, "Unable to close PWM", e);
+            }
+        }
     }
 
     private Runnable mBlinkRunnable = new Runnable() {
@@ -59,4 +96,12 @@ public class MainActivity extends Activity {
             }
         }
     };
+
+    public void initializePwm(Pwm pwm) throws IOException {
+        pwm.setPwmFrequencyHz(120);
+        pwm.setPwmDutyCycle(10);
+
+        // Enable the PWM signal
+        pwm.setEnabled(true);
+    }
 }
